e1b7608711b6bbe5439dca56141b53ebf0965b0f
Alexandrescu Nicolae <nicolae.alexandrescu@info.uaic.ro>
2021-03-04 11:12:54 +0200

    Am adaugat laboratorul 2

---
+++ lab3/Ex2/Ex2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Canvas.h"
+
+int main()
+{
+    Canvas canvas(101, 51);
+    canvas.FillCircle(25, 50, 10, '5');
+    canvas.Clear();
+    canvas.DrawCircle(5, 5, 5, '*');
+    canvas.FillCircle(20, 20, 3, '&');
+    canvas.DrawRect(40, 2, 45, 4, 'x');
+    canvas.FillRect(10, 10, 14, 14, 'o');
+    canvas.SetPoint(25, 50, 'X');
+    canvas.SetPoint(0, 0, '+');
+    canvas.SetPoint(0, 100, '+');
+    canvas.SetPoint(50, 0, '+');
+    canvas.SetPoint(50, 100, '+');
+    canvas.DrawLine(3, 33, 10, 73, 'a');
+    canvas.Print();
+}
